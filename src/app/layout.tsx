0d56f1bbdde2017125b97f0da2477db247cef32b
Farras Adhani Zayn <Farras.FF6@gmail.com>
2026-06-08 15:07:59 +0700

    fix(navbar): update portfolio link to external URL and suppress hydration warning on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
         />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   )
 }
